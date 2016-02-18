1e6785591e6fab3032d6377b5bfeaadd240078d0
Mark H <mmhwang@ucsd.edu>
2016-02-18 21:37:08 +0000

    analytics lab

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -10,4 +10,15 @@ $(document).ready(function() {
  */
 function initializePage() {
 	// your code here
+	//Click listener [analytics]
+	$("button").click(projectClick);
+
+}
+
+function projectClick(e) { 
+    // prevent the page from reloading      
+    e.preventDefault();
+    // In an event handler, $(this) refers to      
+    // the object that triggered the event      
+    ga("send", "event", "like", "click");
 }
